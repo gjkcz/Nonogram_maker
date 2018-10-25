ca664a202aa98f52efd510efa398e9b9549e78af
xvrat01 <xvrat01@gjk.cz>
2018-10-26 00:16:56 +0200

    Improved y_hint, now it's not too limited. Both x and y are functioning

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@
 
 
 def pic():
-    global picture                                # nonogram picture (in 1 and 0)
+    global picture                                                                       # nonogram picture (in 1 and 0)
     global y
     global rotate_pic
     for i in range(0, y):
@@ -15,7 +15,7 @@ def pic():
         picture.append(line1)
     # print(*picture, sep="\n")
 
-    rotate_pic = map(list, zip(*picture))       # rotate picture 90 degrees
+    rotate_pic = list(map(list, zip(*picture)))                                              # rotate picture 90 degrees
     # print(*rotate_pic, sep="\n")
 
 
@@ -27,32 +27,32 @@ def horizontal_hint():
     n = 0
     index = 0
     pos = 0
-    for i in picture:
-        if 1 in picture[index] and 0 in picture[index]:
+    for i in range(len(picture)):
+        if 1 in picture[i] and 0 in picture[i]:
             row_x.clear()
-            for l in picture[index]:
-                if picture[index][pos] == 0:
+            for l in range(len(picture[i])):
+                if picture[i][l] == 0:
                     row_x.append(n)
                     if n != 0:
                         row_x.append(0)
                     n = 0
                     pos += 1
-                elif picture[index][pos] == 1:
+                elif picture[i][l] == 1:
                     n += 1
                     pos += 1
                 if pos == x and n != 0:
                     row_x.append(n)
+                    n = 0
             row_x.append('|')
             hint_x.append(row_x[:])
             pos = 0
-            index += 1
-        elif 1 in picture[index] and 0 not in picture[index]:
+        elif 1 in picture[i] and 0 not in picture[i]:
             row_x.clear()
-            row_x.append(i.count(1))
+            row_x.append(picture[i].count(1))
             row_x.append('|')
             hint_x.append(row_x[:])
             index += 1
-        elif 1 not in picture[index] and 0 in picture[index]:
+        elif 1 not in picture[i] and 0 in picture[i]:
             row_x.clear()
             row_x.append(' ')
             row_x.append('|')
@@ -60,78 +60,89 @@ def horizontal_hint():
             index += 1
 
     index = 0
-    for i in hint_x:  # remove zeros from list
-        if 0 in hint_x[index]:  #
-            while 0 in hint_x[index]:  #
-                hint_x[index].remove(0)  #
+    for i in range(len(hint_x)):                                                                # remove zeros from list
+        if 0 in hint_x[i]:
+            while 0 in hint_x[i]:
+                hint_x[i].remove(0)
         index += 1
 
-    index = 0  # x_max = number of characters in longest row_x
-    for i in hint_x:  #
-        if x_max < len(hint_x[index]):  #
-            x_max = len(hint_x[index])  #
-        index += 1  #
-    x_max = (2 * x_max) - 1
-
-    index = 0
-    for i in hint_x:
-        for n in hint_x[index]:
-            if len(hint_x[index]) * 2 - 1 < x_max:
-                hint_x[index].insert(0, ((x_max - (len(hint_x[index])) - 2) * ' '))
+    index = 0                                                            # x_max = number of characters in longest row_x
+    for i in range(len(hint_x)):
+        if x_max < len(hint_x[i]):
+            x_max = len(hint_x[i])
         index += 1
+
+    for i in range(len(hint_x)):
+        for j in hint_x[i]:
+            n = x_max - len(hint_x[i])
+            if len(hint_x[i]) < x_max:
+                for k in range (n):
+                    hint_x[i].insert(0, ' ')
 
     hint_x = [' '.join([str(c) for c in lst]) for lst in hint_x]
 
 
 def vertical_hint():
+    global rotate_pic
     global hint_y
     global x_max
-    global y_max
     global x
-    for i in rotate_pic:
-        if i.count(0) > 0 and i.count(1) > 0:
-            b = i.index(0)
-            row_y = [sum(i[:b]), sum(i[b:])]
-            row_y = row_y[::-1]                 # reverse the order of row_y,
-            row_y.insert(0, '-')                # add '_' to the beginning of row_y,
-            row_y = row_y[::-1]                 # reverse back the order of row_y
-            hint_y.append(row_y)
-        else:
-            if i.count(1) > 1:
-                row_y = [i.count(1)]
-                row_y = row_y[::-1]
-                row_y.insert(0, '-')
-                row_y = row_y[::-1]
-                hint_y.append(row_y)
-            else:
-                row_y = [' ']
-                row_y = row_y[::-1]
-                row_y.insert(0, '-')
-                row_y = row_y[::-1]
-                hint_y.append(row_y)
+    global y
+    y_max = -1
+    row_y = []
+    pos = 0
+    n = 0
+    for i in range(len(rotate_pic)):
+        if 1 in rotate_pic[i] and 0 in rotate_pic[i]:                     # if 1 in rotate_pic[i] and 0 in rotate_pic[i]
+            row_y.clear()
+            for l in range(len(rotate_pic[i])):
+                if rotate_pic[i][l] == 0:
+                    row_y.append(n)
+                    if n != 0:
+                        row_y.append(0)
+                    n = 0
+                    pos += 1
+                elif rotate_pic[i][l] == 1:
+                    n += 1
+                    pos += 1
+                if pos == y and n != 0:
+                    row_y.append(n)
+                    n = 0
+            row_y.append('_')
+            hint_y.append(row_y[:])
+            pos = 0
+        elif 1 in rotate_pic[i] and 0 not in rotate_pic[i]:
+            row_y.clear()
+            row_y.append(rotate_pic[i].count(1))
+            row_y.append('_')
+            hint_y.append(row_y[:])
+        elif 1 not in rotate_pic[i] and 0 in rotate_pic[i]:
+            row_y.clear()
+            row_y.append(' ')
+            row_y.append('_')
+            hint_y.append(row_y[:])
 
-    index = 0
-    for i in hint_y:                                        # find the longest hint_y sublist
-        if len(hint_y[index]) > y_max:
-            y_max = len(hint_y[index])
-        index += 1
+    for i in range(len(hint_y)):                                                                # remove zeros from list
+        if 0 in hint_y[i]:
+            while 0 in hint_y[i]:
+                hint_y[i].remove(0)
 
-    index = 0
-    for i in hint_y:                                        # add  (y_max - len(hint_y)) * ' ' to the beginning
-        for l in hint_y[index]:
-            n = y_max - len(hint_y[index])
-            if len(hint_y[index]) < y_max:
-                for n in range(n):
-                    hint_y[index].insert(0, ' ')
-        index += 1
+    for i in range(len(hint_y)):                                         # y_max = number of characters in longest row_y
+        if y_max < len(hint_y[i]):
+            y_max = len(hint_y[i])
 
-    hint_y = map(list, zip(*hint_y))
+    for i in range(len(hint_y)):                                     # add  (y_max - len(hint_y)) * ' ' to the beginning
+        for j in hint_y[i]:
+            n = y_max - len(hint_y[i])
+            if len(hint_y[i]) < y_max:
+                for k in range(n):
+                    hint_y[i].insert(0, ' ')
 
-    index = 0
+    hint_y = list(map(list, zip(*hint_y)))
 
-    for n in hint_y:
-        hint_y[index].insert(0, (x_max * ' '))
-        index += 1
+    # for m in range(len(hint_y)):                                                   # add  x_max * ' ' to the beginning
+    #     for n in hint_y[m]:
+    #             hint_y[m].insert(0, (x_max * 2 - 1) * ' ')
 
     hint_y = [' '.join([str(c) for c in lst]) for lst in hint_y]
 
@@ -141,11 +152,10 @@ rotate_pic = []
 hint_x = []
 hint_y = []
 x_max = -1
-y_max = -1
-x = int(input('width? '))                    # input the dimensions of the picture
+x = int(input('width? '))                                                          # input the dimensions of the picture
 y = int(input('height? '))
 pic()
 horizontal_hint()
-# vertical_hint()
-# print(*hint_y, sep="\n")
+vertical_hint()
+print(*hint_y, sep="\n")
 print(*hint_x, sep="\n")
