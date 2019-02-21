2c8d5fd8b69a7759e5911bef8e3a1c63c200ac40
xvrat01 <xvrat01@gjk.cz>
2019-02-21 17:21:03 +0100

    Improved (mainly) output of Main_w_PyQt, added icon + background image

--- Main_w_PyQt5.py
+++ Main_w_PyQt5.py
@@ -40,12 +40,11 @@ class App(QWidget):
         self.setWindowTitle("Nonogram_maker")
         self.setWindowIcon(QIcon("kepler.png"))
         label = QLabel(self)
-        pixmap = QPixmap("icon.png")
+        pixmap = QPixmap("keplerII.png")
         label.setPixmap(pixmap)
         self.resize(pixmap.width(), pixmap.height())
-        self.setGeometry(700, 350, 500, 500)
+        self.setGeometry(700, 350, 546, 546)
         self.show()
-
 
     def path_name(self):
         global name
@@ -58,7 +57,10 @@ class App(QWidget):
         global main_path
         global new_img_path
         global hint_path
-        cesta = os.path.join(os.path.dirname(__file__), "_IMG")
+        main_path = os.path.dirname(__file__)
+        cesta = os.path.join(main_path, "_IMG")
+        if not os.path.exists(cesta):
+            os.makedirs(cesta)
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         filename, _ = QFileDialog.getOpenFileName(self, "Zvolte výchozí obrázek", cesta,
@@ -66,22 +68,26 @@ class App(QWidget):
         if filename:
             _, filename = os.path.split(filename)
             name, type = filename.split(".")
+
             # ▼new_filename = name of new (b&w, pixelated) image
             new_filename = (name + "_nonogram." + type)
+
             # ▼hint_name = name of hint for nonogram
             hint_name = (name + "_hint.txt")
-            main_path = os.path.dirname(__file__)
+
             # ▼img_path = path to original image
             img_path = os.path.join(main_path, "_IMG", filename)
+
             # ▼new_folder_path = path to the specific nonogram folder
             new_folder_path = os.path.join(main_path, name)
             if not os.path.exists(new_folder_path):
                 os.makedirs(new_folder_path)
+
             # ▼new_img_path = path to the new image
             new_img_path = os.path.join(new_folder_path, new_filename)
+
             # ▼hint_path = path to nonogram hint
             hint_path = os.path.join(new_folder_path, hint_name)
-
 
     def open_resize(self):
         global img
@@ -90,13 +96,13 @@ class App(QWidget):
         global x
         # ▼open picture and make it grayscale
         img = Image.open(img_path).convert("L")
-        text = "Obrázek má rozlišení " + str(img.size[0]) + "x" + str(img.size[1]) + "px. Přejete si změnit?"
+        text = "Obrázek má rozlišení " + str(img.size[0]) + "x" + str(img.size[1]) + "px. Přejete si změnit?" + "\n"\
+               + "Zadejte požadovanou hodnotu x"
         res_x, okpressed = QInputDialog.getInt(self, "Nonogram_maker", text, 0, 0, 100, 1)
         if okpressed and res_x != 0:
             y = int((img.size[1] / (img.size[0] / res_x)))
             x = res_x
             img = img.resize((res_x, y))
-
 
     def baw_pic(self):
         global img
@@ -105,8 +111,6 @@ class App(QWidget):
         baw = img.point(lambda x: 0 if x < 128 else 255, '1')
         # baw = baw.crop(baw.getbbox())
         baw.save(new_img_path)
-        # baw.show()
-
 
     def baw_to_array(self):
         global res_x
@@ -124,17 +128,14 @@ class App(QWidget):
         picture = img_list.tolist()
         rotate_pic = list(map(list, zip(*picture)))
 
-
     def horizontal_hint(self):
         global picture
         global hint_x
         global x_max
-        global more_digits
-        x_max = -1
-        more_digits = 0
+        qx_max = 0
+        x_max = 0
         row_x = []
         n = 0
-        index = 0
         pos = 0
         for i in range(len(picture)):
             if 1 in picture[i] and 0 in picture[i]:
@@ -158,38 +159,41 @@ class App(QWidget):
                 row_x.clear()
                 row_x.append(picture[i].count(1))
                 hint_x.append(row_x[:])
-                index += 1
             elif 1 not in picture[i] and 0 in picture[i]:
                 row_x.clear()
                 row_x.append(' ')
                 hint_x.append(row_x[:])
-                index += 1
 
         # ▼remove zeros from list
-        index = 0
         for i in range(len(hint_x)):
             if 0 in hint_x[i]:
                 while 0 in hint_x[i]:
                     hint_x[i].remove(0)
-            index += 1
 
         # ▼x_max = number of characters in longest row_x
-        index = 0
         for i in range(len(hint_x)):
-            if x_max < len(hint_x[i]):
-                x_max = len(hint_x[i])
-            index += 1
+            for k in range(len(hint_x[i])):
+                if (hint_x[i][k]) < 10:
+                    qx_max += 2
+                if (hint_x[i][k]) >= 10 and (hint_x[i][k]) < 100:
+                    qx_max += 3
+                if (hint_x[i][k]) >= 100:
+                    qx_max += 4
+            if x_max < qx_max:
+                x_max = qx_max
+            qx_max = 0
 
-        # ▼add spaces to hint_x lines
-        for i in range(len(hint_x)):
-            for j in hint_x[i]:
-                n = x_max - len(hint_x[i])
-                if len(hint_x[i]) < x_max:
-                    for k in range(n):
-                        hint_x[i].insert(0, ' ')
+        x_max -= 1
+
+        # # ▼add spaces to hint_x lines
+        # for i in range(len(hint_x)):
+        #     for j in hint_x[i]:
+        #         n = x_max - len(hint_x[i])
+        #         if len(hint_x[i]) < x_max:
+        #             for k in range(n):
+        #                 hint_x[i].insert(0, ' ')
 
         hint_x = [' '.join([str(c) for c in lst]) for lst in hint_x]
-
 
     def vertical_hint(self):
         global rotate_pic
@@ -197,6 +201,8 @@ class App(QWidget):
         global x_max
         global x
         global y
+        global digits
+        digits = 0
         y_max = -1
         row_y = []
         pos = 0
@@ -239,9 +245,15 @@ class App(QWidget):
             if y_max < len(hint_y[i]):
                 y_max = len(hint_y[i])
 
-        # ▼add  (y_max - len(hint_y)) * ' ' to the beginning
+        # ▼inspect number of digits; add (y_max - len(hint_y)) * ' ' to the beginning
         for i in range(len(hint_y)):
-            for j in hint_y[i]:
+            for j in range(len((hint_y[i]))):
+                if (hint_y[i][j]) in range (1, 9) and digits < 1:
+                    digits = 1
+                elif (hint_y[i][j]) in range(10, 99) and digits < 2:
+                    digits = 2
+                elif (hint_y[i][j]) in range (100, 9999) and digits < 4:
+                    digits = 4
                 n = y_max - len(hint_y[i])
                 if len(hint_y[i]) < y_max:
                     for k in range(n):
@@ -250,22 +262,37 @@ class App(QWidget):
         hint_y = list(map(list, zip(*hint_y)))
 
         # ▼add  x_max * ' ' to the beginning
-        for m in range(len(hint_y)):
-            for n in range(x_max):
-                    hint_y[m].insert(0, ' ')
+        # for m in range(len(hint_y)):
+        #     for n in range(x_max):
+        #             hint_y[m].insert(0, x_max * ' ')
 
-        hint_y = [' '.join([str(c) for c in lst]) for lst in hint_y]
-
+        # hint_y = [' '.join([str(c) for c in lst]) for lst in hint_y]
 
     def print(self):
-        print(*hint_y, sep='\n')
-        print(*hint_x, sep='|' + x * '_|' + '\n', end='|' + x * '_|')
+        print((x_max + 1)* " ", end = "")
+        for i in range(len(hint_y)):
+            for j in hint_y[i]:
+                print("{:<{space}}".format(j, space=digits),end=" ")
+            if i < len(hint_y)-1:
+                print("\n", end=(x_max + 1)* " ")
+            else:
+                print("\n", end="")
+        for i in hint_x:
+            print("{:>{space}}".format(i, space=x_max) + "|" + x * (digits * "_" + "|"))
+
         # ▼print hint to file
         file = open(hint_path, "a")
-        print(*hint_y, sep='\n', file=file)
-        print(*hint_x, sep='|' + x * '_|' + '\n', end='|' + x * '_|', file=file)
+        print((x_max + 1) * " ", end="", file=file)
+        for i in range(len(hint_y)):
+            for j in hint_y[i]:
+                print("{:<{space}}".format(j, space=digits), end=" ", file=file)
+            if i < len(hint_y) - 1:
+                print("\n", end=(x_max + 1) * " ", file=file)
+            else:
+                print("\n", end="", file=file)
+        for i in hint_x:
+            print("{:>{space}}".format(i, space=x_max) + "|" + x * (digits * "_" + "|"),file=file)
         file.close()
-
 
     def repeat(self):
         question = QMessageBox.question(self, 'Nonogram_maker', "Chcete vytvořit další nonogram?",
